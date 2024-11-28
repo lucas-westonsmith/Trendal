39d9c07e0a15a9aaebe9ffbdbe33d0acd7252a96
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2024-11-28 18:27:23 +0000

    graphs good start

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,5 +1,4 @@
 class TrendsController < ApplicationController
-
   def index
     @keyword = params[:keyword]
     scraper = YoutubeScraper.new(@keyword)
@@ -7,11 +6,9 @@ class TrendsController < ApplicationController
     # Fetch trends based on the platform parameter, defaulting to TikTok
     if params[:platform] == 'youtube'
       @youtube_trends = Trend.where(platform: 'youtube')
-
       @tiktok_trends = nil
     else
-      @tiktok_trends = Trend.where(platform: 'tiktok')
-                            .order(:rank) # Order by rank
+      @tiktok_trends = Trend.where(platform: 'tiktok').order(:rank) # Order by rank
       @youtube_trends = nil
     end
   end
@@ -24,6 +21,29 @@ class TrendsController < ApplicationController
 
     Rails.logger.debug "Selected Country: #{@selected_country}, Selected Period: #{@selected_period}"
 
+    # Préparer les données pour le graphique
+    @graph_data_7_days = {}
+    @graph_data_30_days = {}
+    @graph_data_120_days = {}
+
+    # Regrouper les counts par pays et période
+    @trend.counts.group_by(&:country).each do |country, counts|
+      # Pour chaque pays, on récupère les counts pour chaque période (7, 30, 120 jours)
+      counts.each do |count|
+        country_name_with_flag = count.country_name_with_flag # Utilise la méthode pour avoir le nom complet et le drapeau
+
+        case count.period
+        when 7
+          @graph_data_7_days[country_name_with_flag] = count.number
+        when 30
+          @graph_data_30_days[country_name_with_flag] = count.number
+        when 120
+          @graph_data_120_days[country_name_with_flag] = count.number
+        end
+      end
+    end
+
+    # Filtrer les vidéos en fonction du pays et de la période si spécifié
     if @selected_country.present? && @selected_period.present?
       @filtered_videos = @trend.videos.joins(:count)
                                       .where(counts: { country: @selected_country, period: @selected_period })
@@ -33,6 +53,4 @@ class TrendsController < ApplicationController
       @filtered_videos = @trend.videos
     end
   end
-
-
 end
