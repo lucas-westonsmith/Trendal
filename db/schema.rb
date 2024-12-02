97233ade4a1a19e78ae0897baf66e128c80eff5c
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2024-12-02 15:59:23 +0000

    I stop there it is good

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_02_155757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
     t.index ["trend_id"], name: "index_favorites_trends_on_trend_id"
   end
 
+  create_table "keyword_examples", force: :cascade do |t|
+    t.bigint "trend_id", null: false
+    t.text "keyword_phrase"
+    t.string "most_used_as"
+    t.float "ctr"
+    t.text "related_videos"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trend_id"], name: "index_keyword_examples_on_trend_id"
+  end
+
   create_table "prediction_trends", force: :cascade do |t|
     t.bigint "trend_id", null: false
     t.bigint "prediction_id", null: false
@@ -85,6 +96,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "related_hashtags", force: :cascade do |t|
+    t.bigint "trend_id", null: false
+    t.integer "rank"
+    t.string "title"
+    t.integer "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trend_id"], name: "index_related_hashtags_on_trend_id"
+  end
+
   create_table "related_interests", force: :cascade do |t|
     t.bigint "trend_id", null: false
     t.bigint "count_id", null: false
@@ -94,6 +115,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
     t.integer "score"
     t.index ["count_id"], name: "index_related_interests_on_count_id"
     t.index ["trend_id"], name: "index_related_interests_on_trend_id"
+  end
+
+  create_table "related_keywords", force: :cascade do |t|
+    t.bigint "trend_id", null: false
+    t.integer "rank"
+    t.string "title"
+    t.integer "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trend_id"], name: "index_related_keywords_on_trend_id"
   end
 
   create_table "trends", force: :cascade do |t|
@@ -116,7 +147,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
     t.datetime "published_at"
     t.string "channel_name"
     t.string "video_url"
-
     t.boolean "display"
     t.string "tiktok_page"
     t.integer "popularity"
@@ -162,10 +192,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_100222) do
   add_foreign_key "favorites", "users"
   add_foreign_key "favorites_trends", "favorites"
   add_foreign_key "favorites_trends", "trends"
+  add_foreign_key "keyword_examples", "trends"
   add_foreign_key "prediction_trends", "predictions"
   add_foreign_key "prediction_trends", "trends"
+  add_foreign_key "related_hashtags", "trends"
   add_foreign_key "related_interests", "counts"
   add_foreign_key "related_interests", "trends"
+  add_foreign_key "related_keywords", "trends"
   add_foreign_key "videos", "counts"
   add_foreign_key "videos", "trends"
 end
---
+++ test/models/keyword_example_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class KeywordExampleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
