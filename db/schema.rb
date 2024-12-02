c5fb8b36fa511b73ee25df18fdec90ff38f263ff
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2024-12-02 09:39:41 +0000

    Should now work when you save your favorites and scrape a new time thanks to the attribute display that has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_01_223825) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_02_085401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,6 +116,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_01_223825) do
     t.datetime "published_at"
     t.string "channel_name"
     t.string "video_url"
+    t.boolean "display"
   end
 
   create_table "users", force: :cascade do |t|
