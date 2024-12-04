e7b331c8f0604da8d2e272c077ca3086871b7a3d
Artur Inoue <arturcorreainoue@gmail.com>
2024-12-04 16:17:39 +0000

    animations

--- app/javascript/controllers/homepage_controller.js
+++ app/javascript/controllers/homepage_controller.js
@@ -3,19 +3,20 @@ import gsap from "gsap";
 
 export default class extends Controller {
   connect() {
-    // Apply animation to elements within the .gradient section
+    // Apply animation to text elements with .animate-text class
     gsap.from(".animate-text", {
       opacity: 0,
       y: 20,
       duration: 1,
-      stagger: 0.3
+      stagger: 0.3 // Stagger the animation of multiple elements
     });
 
+    // Apply animation to buttons with .animate-button class
     gsap.from(".animate-button", {
       opacity: 0,
       y: 30,
       duration: 1,
-      delay: 1,
+      delay: 1, // Delay the button animation to start after text
     });
   }
 }
